864bb4e5594e0cd455af4bc546840ae0be7875f1
javiducun <javiducun@gmail.com>
2023-06-02 17:37:14 +0200

    Se muestra un gif en el documento

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,16 @@
+import "./styles.css"
+
+export const Home = () => {
+  return (
+    <>
+      <header>Guifaffinity</header>
+      <main>
+        <img
+          src="https://media1.giphy.com/media/UOsYvXGW1UFlS/giphy.gif?cid=ecf05e47c5avk3rlidaqt7zce2rl6u99kgd2hx9lknvjqhm4&ep=v1_gifs_search&rid=giphy.gif&ct=g"
+          alt="gatitos locos"
+        />
+      </main>
+      <footer>Guifaffinity</footer>
+    </>
+  )
+}
