8bd1b10edb1faecaa6a9181f2efceeaa19202685
javiducun <javiducun@gmail.com>
2023-06-09 17:44:55 +0200

    ejercicio practio useToggle

---
+++ src/hooks/useToggle.ts
@@ -0,0 +1,14 @@
+// Gestiona una variable booleana llamada value
+// tiene una funcion toggle que cambia el estado de esta variable a su opuesto
+// el hook expone la variable creada y la funcion toggle
+
+import { useState } from "react"
+
+const useToggle = (initialValue: boolean = false) => {
+  const [value, setValue] = useState<boolean>(initialValue)
+
+  const toggle = () => setValue(!value)
+
+  return { value, toggle }
+}
+export default useToggle
