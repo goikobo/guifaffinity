ddbd3020c801723948fd8ba74cae98a25e2da755
Goiko <juanmiguel.goicoechea@biko2.com>
2023-06-13 18:36:25 +0200

    anadido icono searchIcon

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,6 +2,7 @@ import { FC } from "react";
 import useTrending from "../../hooks/useTrending";
 import "./styles.css";
 import { GifGrid } from "../../components/GifGrid/GifGrid";
+import searchIcon from "../../res/icons/search.svg";
 
 export const Home: FC = () => {
   const { trendingGifs } = useTrending();
@@ -15,6 +16,7 @@ export const Home: FC = () => {
       <main>
         <section>
           <input placeholder="¿Que quieres buscar? ¡Encuentralo!" />
+          <img src={searchIcon} alt="Search Icon" />
         </section>
         <GifGrid gifs={trendingGifs} />
       </main>
