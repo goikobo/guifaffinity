a5360e2028c62f52912d1abb72e66543adeb7cc0
Goiko <juanmiguel.goicoechea@biko2.com>
2023-06-13 18:12:35 +0200

    anadido test se muestra texto busqueda

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,35 +1,42 @@
-import React from "react"
+import React from "react";
 import {
   render,
   screen,
   waitForElementToBeRemoved,
-} from "@testing-library/react"
-import App from "../App"
+} from "@testing-library/react";
+import App from "../App";
 
 test("Renderiza un gif", async () => {
-  render(<App />)
-  await waitForElementToBeRemoved(() => screen.queryByText("Cargando..."))
-  const linkElement = screen.getByAltText(/gatitos locos/i)
-  expect(linkElement).toBeVisible()
-})
+  render(<App />);
+  await waitForElementToBeRemoved(() => screen.queryByText("Cargando..."));
+  const linkElement = screen.getByAltText(/gatitos locos/i);
+  expect(linkElement).toBeVisible();
+});
 
 test("Muestra hasta 20 gifs", async () => {
-  render(<App />)
-  await waitForElementToBeRemoved(() => screen.queryByText("Cargando..."))
-  const elements = screen.getAllByTestId("gif-card")
-  expect(elements.length).toBeLessThanOrEqual(20)
-})
+  render(<App />);
+  await waitForElementToBeRemoved(() => screen.queryByText("Cargando..."));
+  const elements = screen.getAllByTestId("gif-card");
+  expect(elements.length).toBeLessThanOrEqual(20);
+});
 
 test("Muestra mensaje de trendings", async () => {
-  render(<App />)
-  await waitForElementToBeRemoved(() => screen.queryByText("Cargando..."))
-  const elements = screen.getByText("Los guif más trendings del momento")
-  expect(elements).toBeVisible()
-})
+  render(<App />);
+  await waitForElementToBeRemoved(() => screen.queryByText("Cargando..."));
+  const elements = screen.getByText("Los guif más trendings del momento");
+  expect(elements).toBeVisible();
+});
 
 test("Muestra icono de trendings", async () => {
-  render(<App />)
-  await waitForElementToBeRemoved(() => screen.queryByText("Cargando..."))
-  const elements = screen.getByAltText("Arrow Icon")
-  expect(elements).toBeVisible()
-})
+  render(<App />);
+  await waitForElementToBeRemoved(() => screen.queryByText("Cargando..."));
+  const elements = screen.getByAltText("Arrow Icon");
+  expect(elements).toBeVisible();
+});
+
+test("Muestra texto de busqueda", async () => {
+  render(<App />);
+  await waitForElementToBeRemoved(() => screen.queryByText("Cargando..."));
+  const elements = screen.getByText("¿Que quieres buscar? ¡Encuentralo!");
+  expect(elements).toBeVisible();
+});
