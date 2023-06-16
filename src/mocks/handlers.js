bd3f6c805b2557fc5af37114bab7ed48b42dabb1
Goiko <juanmiguel.goicoechea@biko2.com>
2023-06-16 17:06:57 +0200

    ev: anadidas las variables de entorno

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -1,10 +1,8 @@
 // src/mocks/handlers.js
-import { rest } from "msw"
-import { trendingResolver } from "./resolvers/trendingResolver"
+import { rest } from "msw";
+import { trendingResolver } from "./resolvers/trendingResolver";
+const apiUrl = process.env.REACT_APP_API_URL;
 
 export const handlers = [
-  rest.get(
-    "https://guifaffinity/api/trending?limit=20&offset=0",
-    trendingResolver
-  ),
-]
+  rest.get(`${apiUrl}/memes?limit=50&offset=0`, trendingResolver),
+];
--- src/services/getTrendingGifs.ts
+++ src/services/getTrendingGifs.ts
@@ -1,8 +1,9 @@
-import { Gif } from "../interfaces/Gif"
+import { Gif } from "../interfaces/Gif";
 
 export async function getTrendingGifs(): Promise<Gif[]> {
-  const url = `https://guifaffinity/api/trending?limit=20&offset=0`
-  const res = await fetch(url)
-  const data = await res.json()
-  return data.result
+  const apiUrl = process.env.REACT_APP_API_URL;
+  const url = `${apiUrl}/memes?limit=50&offset=0`;
+  const res = await fetch(url);
+  const data = await res.json();
+  return data.result;
 }
