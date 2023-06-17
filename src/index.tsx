2002ba94f47e485ed4850af673d1a3fb2d306f58
javiducun <javiducun@gmail.com>
2023-06-17 12:11:57 +0200

    anadido npm run start-api

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,19 @@
-import "./styles/reset.css";
-import "./styles/variables.css";
+import "./styles/reset.css"
+import "./styles/variables.css"
 
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
+import React from "react"
+import ReactDOM from "react-dom/client"
+import "./index.css"
+import App from "./App"
 
-if (process.env.NODE_ENV === "development") {
-  const { worker } = require("./mocks/browser");
-  worker.start();
+if (!process.env.REACT_APP_USE_REMOTE_API) {
+  const { worker } = require("./mocks/browser")
+  worker.start()
 }
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-);
+)
