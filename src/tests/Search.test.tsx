8feb30e14c41622321d6d77b50cfd4decebaffc5
javiducun <javiducun@gmail.com>
2023-06-27 19:18:22 +0200

    anadido test buscar los gatitos

--- src/tests/Search.test.tsx
+++ src/tests/Search.test.tsx
@@ -3,21 +3,24 @@ import {
   screen,
   waitForElementToBeRemoved,
   fireEvent,
-} from "@testing-library/react";
-import App from "../App";
+} from "@testing-library/react"
+import App from "../App"
 
-test("Search dogs", async () => {
-  render(<App />);
-  await waitForElementToBeRemoved(() => screen.queryByText("Cargando..."));
+test("Search cats", async () => {
+  render(<App />)
+  await waitForElementToBeRemoved(() => screen.queryByText("Cargando..."))
   const input = screen.getByPlaceholderText(
     "¿Que quieres buscar? ¡Encuentralo!"
-  );
-  fireEvent.change(input, { target: { value: "cat" } });
+  )
+  fireEvent.change(input, { target: { value: "cat" } })
 
-  const button = screen.getByAltText("Search Icon");
-  fireEvent.click(button);
+  const button = screen.getByAltText("Search Icon")
+  fireEvent.click(button)
 
-  const elements = screen.getAllByTestId("gif-card");
-  // TODO: expect all to contains cat in alt text
-  expect(true).toBeFalsy();
-});
+  await waitForElementToBeRemoved(() => screen.queryByText("Cargando..."))
+
+  const elements = screen.getAllByTestId("gif-card")
+  elements.forEach((element) => {
+    expect(element.getAttribute("aria-label")).toContain(/cat/i)
+  })
+})
