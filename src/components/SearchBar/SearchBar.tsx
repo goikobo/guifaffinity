a3cc968ed6ce30b4df31ae165aa0bbea8c601a5f
javiducun <javiducun@gmail.com>
2023-06-30 15:55:32 +0200

    gestion de estado de la barra de busqueda

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,17 +1,19 @@
-import { FC } from "react"
+import { FC, useState } from "react"
 import "./styles.css"
 import searchIcon from "../../res/icons/search.svg"
 
 export const SearchBar: FC<{ OnSearch: (text: string) => void }> = ({
   OnSearch,
 }) => {
+  const [searchedText, setSearchedTest] = useState<string>("")
   return (
     <section className="search-container">
       <input
         id="search-text-input"
         placeholder="¿Que quieres buscar? ¡Encuentralo!"
+        onChange={(args) => setSearchedTest(args.target.value)}
       />
-      <button id="search-btn" onClick={() => OnSearch("cat")}>
+      <button id="search-btn" onClick={() => OnSearch(searchedText)}>
         <img className="search-icon" src={searchIcon} alt="Search Icon" />
       </button>
     </section>
