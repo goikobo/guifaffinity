7810d594dd54a6b6815e4139eaeed089411268a3
javiducun <javiducun@gmail.com>
2023-06-30 16:06:15 +0200

    cambiar endpoint memes por gif

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -1,10 +1,10 @@
 // src/mocks/handlers.js
-import { rest } from "msw";
-import { trendingResolver } from "./resolvers/trendingResolver";
-import { searchResolver } from "./resolvers/searchResolver";
-const apiUrl = process.env.REACT_APP_API_URL;
+import { rest } from "msw"
+import { trendingResolver } from "./resolvers/trendingResolver"
+import { searchResolver } from "./resolvers/searchResolver"
+const apiUrl = process.env.REACT_APP_API_URL
 
 export const handlers = [
-  rest.get(`${apiUrl}/memes`, trendingResolver),
-  rest.get(`${apiUrl}/memes/search`, searchResolver),
-];
+  rest.get(`${apiUrl}/gifs`, trendingResolver),
+  rest.get(`${apiUrl}/gifs/search`, searchResolver),
+]
--- src/services/getTrendingGifs.ts
+++ src/services/getTrendingGifs.ts
@@ -3,7 +3,7 @@ import { Gif } from "../interfaces/Gif"
 
 export async function getTrendingGifs(): Promise<Gif[]> {
   const apiUrl = process.env.REACT_APP_API_URL
-  const url = `${apiUrl}/memes?limit=50&offset=0`
+  const url = `${apiUrl}/gifs?limit=50&offset=0`
   const res = await fetch(url)
   const data: GifDTO[] = await res.json()
   return data.map(mapGuif)
