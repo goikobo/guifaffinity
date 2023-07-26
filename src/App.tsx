412fa2a1667b9f73c44d0e3d57713da013d508e4
javiducun <javiducun@gmail.com>
2023-07-26 17:49:52 +0200

    add react router to project

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import { Home } from "./pages/Home/Home";
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/asd" element={<div>asd</div>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
