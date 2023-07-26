0941eebe5806cb2e7286dce2ea0a962b1bb663c4
javiducun <javiducun@gmail.com>
2023-07-26 17:57:15 +0200

    add GifDetail page

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,26 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useParams } from "react-router-dom";
 import "./App.css";
 import { Home } from "./pages/Home/Home";
 
 function App() {
   return (
     <div className="App">
-      <Router>
+      <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/asd" element={<div>asd</div>} />
+          <Route path="/detail/:id" element={<GifDetail />} />
         </Routes>
-      </Router>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+function GifDetail() {
+  let { id } = useParams();
+
+  return (
+    <div>
+      <h3>ID: {id}</h3>
     </div>
   );
 }
