8edc71e7d0242114aa4cd98e688634884664167f
Goiko <juanmiguel.goicoechea@biko2.com>
2023-07-28 20:20:58 +0200

    change mock run

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
-if (!process.env.REACT_APP_USE_REMOTE_API) {
+if (process.env.REACT_APP_USE_MOCK_API) {
   const { worker } = require("./mocks/browser");
   worker.start();
 }
