58505c206f9c862d5ea65fd7a292d8a9114449c9
Goiko <juanmiguel.goicoechea@biko2.com>
2023-07-28 20:08:23 +0200

    add go to home from heeader

--- src/components/Title/Title.tsx
+++ src/components/Title/Title.tsx
@@ -1,12 +1,13 @@
 import { FC } from "react";
 import "./styles.css";
 import titleIcon from "../../res/icons/app-icon.svg";
+import { Link } from "react-router-dom";
 
 export const Title: FC<{}> = () => {
   return (
-    <section className="title-container">
+    <Link className="title-container" to="">
       <img className="title-icon" src={titleIcon} alt="Guifaffinity-icon" />
       <h1>Guifaffinity</h1>
-    </section>
+    </Link>
   );
 };
