0d6f9ab0b750617c3dc9e3f67eac81127fa96d4c
Goiko <juanmiguel.goicoechea@biko2.com>
2023-07-28 19:40:14 +0200

    add gif image

--- src/pages/GifDetail/GifDetail.tsx
+++ src/pages/GifDetail/GifDetail.tsx
@@ -3,6 +3,7 @@ import "./styles.css";
 import { useParams } from "react-router-dom";
 import useGif from "../../hooks/useGif";
 import { GifRepository } from "../../core/domain/Gif/GifRepository";
+import { GifCard } from "../../components/GifCard/GifCard";
 
 export const GifDetail: FC<{ gifRepository: GifRepository }> = ({
   gifRepository,
@@ -10,14 +11,20 @@ export const GifDetail: FC<{ gifRepository: GifRepository }> = ({
   const { id } = useParams();
   const { gif } = useGif(gifRepository, id);
 
+  if (!gif) {
+    return <div></div>;
+  }
   return (
     <>
       <main>
         <div>
           <h2>
             {gif?.title.split(" by ")[0]}
-            <span className="author"> by {gif?.user.display_name}</span>
+            <span className="author"> by {gif.user.display_name}</span>
           </h2>
+          <section className="gif-container">
+            <GifCard trendingGif={gif}></GifCard>
+          </section>
         </div>
       </main>
     </>
